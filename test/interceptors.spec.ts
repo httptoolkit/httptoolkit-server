6a193835147c4d379102805871ed93e994dc7e59
Tim Perry <pimterry@gmail.com>
2018-11-03 12:35:32 +0100

    Test that Chrome makes real & successful requests

--- test/interceptors.spec.ts
+++ test/interceptors.spec.ts
@@ -1,28 +1,68 @@
+import * as _ from 'lodash';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as tmp from 'tmp';
-
-import { buildInterceptors } from '../src/interceptors';
 import { expect } from 'chai';
+import { getLocal, CompletedRequest, generateCACertificate, Mockttp } from 'mockttp';
+
+import { buildInterceptors, Interceptor } from '../src/interceptors';
 
 describe('Fresh chrome interceptor', () => {
 
-    const configPath = tmp.dirSync({ unsafeCleanup: true }).name;
-    const interceptors = buildInterceptors({ configPath });
+    let server: Mockttp;
+    let freshChrome: Interceptor;
+
+    before(function () {
+        this.timeout(10000);
+
+        const configPath = tmp.dirSync({ unsafeCleanup: true }).name;
+
+        const keyPath = path.join(configPath, 'ca.key');
+        const certPath = path.join(configPath, 'ca.pem');
+        const newCertPair = generateCACertificate({ commonName: 'HTTP Toolkit CA - DO NOT TRUST' });
+        fs.writeFileSync(keyPath, newCertPair.key);
+        fs.writeFileSync(certPath, newCertPair.cert);
+
+        server = getLocal({ https: { certPath, keyPath } });
+        const interceptors = buildInterceptors({ configPath });
+        freshChrome = interceptors['fresh-chrome']
+    });
+
+    beforeEach(() => server.start());
+    afterEach(async () => {
+        await freshChrome.deactivate(server.port);
+        await server.stop();
+    });
 
     it('is available', async () => {
-        const freshChrome = interceptors['fresh-chrome'];
-
         expect(await freshChrome.isActivable()).to.equal(true);
     });
 
     it('can be activated', async () => {
-        const freshChrome = interceptors['fresh-chrome'];
-        expect(freshChrome.isActive(8000)).to.equal(false);
+        expect(freshChrome.isActive(server.port)).to.equal(false);
 
-        await freshChrome.activate(8000);
-        expect(freshChrome.isActive(8000)).to.equal(true);
-        expect(freshChrome.isActive(9000)).to.equal(false);
+        await freshChrome.activate(server.port);
+        expect(freshChrome.isActive(server.port)).to.equal(true);
+        expect(freshChrome.isActive(server.port + 1)).to.equal(false);
 
-        await freshChrome.deactivate(8000);
-        expect(freshChrome.isActive(8000)).to.equal(false);
+        await freshChrome.deactivate(server.port);
+        expect(freshChrome.isActive(server.port)).to.equal(false);
+    });
+
+    it('successfully makes requests', async () => {
+        await server.anyRequest().thenPassThrough();
+
+        const exampleRequestReceived = new Promise<CompletedRequest>((resolve) =>
+            server.on('request', (req) => {
+                if (req.url.startsWith('https://example.com')) {
+                    resolve(req);
+                }
+            })
+        );
+
+        await freshChrome.activate(server.port);
+
+        // Only resolves if example.com request is sent successfully
+        await exampleRequestReceived;
     });
 });
