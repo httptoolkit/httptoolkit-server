436dbe0148bce9ddbdeb9cc64913625d5c06982b
Tim Perry <pimterry@gmail.com>
2019-01-14 17:44:43 +0100

    Report errors to Sentry

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,3 +1,16 @@
+import * as Sentry from '@sentry/node';
+
+const packageJson = require('../../package.json');
+let { SENTRY_DSN } = process.env;
+if (!SENTRY_DSN && process.env.HTTPTOOLKIT_SERVER_BINPATH) {
+    // If we're a built binary, use the standard DSN automatically
+    SENTRY_DSN = 'https://f6775276f60042bea6d5e951ca1d0e91@sentry.io/1371158';
+}
+
+if (SENTRY_DSN) {
+    Sentry.init({ dsn: SENTRY_DSN, release: packageJson.version });
+}
+
 import { Command, flags } from '@oclif/command'
 import { runHTK } from '../index';
 
