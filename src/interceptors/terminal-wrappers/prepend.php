d909462d5196ecf764195e0980c2b49252517de9
Tim Perry <pimterry@gmail.com>
2019-05-21 12:40:53 +0200

    Ensure PHP prepending also runs any existing prepend scripts

--- src/interceptors/terminal-wrappers/prepend.php
+++ src/interceptors/terminal-wrappers/prepend.php
@@ -4,4 +4,15 @@
             'http' => array('proxy' => 'localhost:8000')
         )
     );
+
+    // We've overridden php.ini to ensure this runs. We should go back to
+    // php.ini, check if there was a previous value, and ensure that
+    // gets run as well, to make sure we don't break anything.
+    $phpIniLocation = php_ini_loaded_file();
+    if ($phpIniLocation) {
+        $phpIniContents = parse_ini_file($phpIniLocation);
+        if ($phpIniContents['auto_prepend_file']) {
+            require($phpIniContents['auto_prepend_file']);
+        }
+    }
 ?>
