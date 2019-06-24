734207d3ea4f5a6067167e6feb7ad8a478699be6
Tim Perry <pimterry@gmail.com>
2019-06-24 16:27:51 -0400

    Inject proxy into Node 10+ using global-agent

--- src/interceptors/fresh-terminal.ts
+++ src/interceptors/fresh-terminal.ts
@@ -251,6 +251,8 @@ export class TerminalInterceptor implements Interceptor {
                 'HTTP_PROXY': `http://localhost:${proxyPort}`,
                 'https_proxy': `http://localhost:${proxyPort}`,
                 'HTTPS_PROXY': `http://localhost:${proxyPort}`,
+                // Used by global-agent to configure node.js HTTP(S) defaults
+                'GLOBAL_AGENT_HTTP_PROXY': `http://localhost:${proxyPort}`,
                 // Used by some CGI engines to avoid 'httpoxy' vulnerability
                 'CGI_HTTP_PROXY': `http://localhost:${proxyPort}`,
 
