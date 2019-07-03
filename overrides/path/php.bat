ccfabb0736c9132bd92f638b523ea86afe80ddf8
Tim Perry <pimterry@gmail.com>
2019-07-02 20:50:30 -0400

    Make sure php inherits the correct overridden PATH, in case of subprocesses

--- overrides/path/php.bat
+++ overrides/path/php.bat
@@ -10,5 +10,11 @@ set WRAPPER_FOLDER=%THIS_PATH:~0,-1%
 REM Remove that folder from PATH
 call set PATH=%%PATH:%WRAPPER_FOLDER%;=%%
 
+REM Get the real php path, store it in %REAL_PHP%
+FOR /F "tokens=*" %%g IN ('where php') do (SET REAL_PHP=%%g)
+
+REM Reset PATH, so its visible to php & subprocesses
+set PATH=%ORIGINALPATH%
+
 REM Start PHP for real, with extra args to override certain configs
-php -d "openssl.cafile=%SSL_CERT_FILE%" -d "curl.cainfo=%SSL_CERT_FILE%" -d "auto_prepend_file=%WRAPPER_FOLDER%\prepend.php" %*
+"%REAL_PHP%" -d "openssl.cafile=%SSL_CERT_FILE%" -d "curl.cainfo=%SSL_CERT_FILE%" -d "auto_prepend_file=%WRAPPER_FOLDER%\prepend.php" %*
