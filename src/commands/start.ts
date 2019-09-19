b1359a16c6c9341b1136bf063f8bb8e42abf09a1
Tim Perry <pimterry@gmail.com>
2019-09-19 15:42:30 +0200

    Build a bundled CLI with webpack for performance
    This reduces startup time on my machine by approx 40%, likely much more
    on machines with non-SSDs or active AV. Will also allow us (shortly) to
    dramatically shrink the deployed server size too.

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,8 +1,26 @@
 import { initErrorTracking } from '../error-tracking';
 initErrorTracking();
 
+import { IS_PROD_BUILD } from '../util';
+
 import { Command, flags } from '@oclif/command'
-import { runHTK } from '../index';
+
+import { runHTK as runHTKFunction } from '../index';
+
+let runHTK: typeof runHTKFunction;
+if (IS_PROD_BUILD || process.env.OCLIF_TS_NODE === '0') {
+    // Full package: try to explicitly load the bundle
+    try {
+        ({ runHTK } = require('../../bundle/index'));
+    } catch (e) {
+        // Fallback (bundle is not yet included in real package)
+        console.log('Could not load bundle, loading raw');
+        ({ runHTK } = require('../index'));
+    }
+} else {
+    // Npm or dev: run the raw code
+    ({ runHTK } = require('../index'));
+}
 
 class HttpToolkitServer extends Command {
     static description = 'start the HTTP Toolkit server'
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'bundle'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    mode: 'production',
+    devtool: 'source-map',
+    target: 'node',
+    node: {
+        __dirname: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                // Required to build .mjs files correctly
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: "javascript/auto",
+            },
+            {
+                // Allows us to require native modules
+                test: /\.node$/,
+                loader: "native-ext-loader"
+            }
+        ]
+    },
+    externals: [
+        '@oclif/plugin-update/lib/commands/update'
+    ],
+    plugins: [
+        // Optimistic require for 'iconv' in 'encoding', falls back to 'iconv-lite'
+        new webpack.IgnorePlugin(/^iconv$/),
+        // Optimistically required in (various) ws versions, with fallback
+        new webpack.IgnorePlugin(/^bufferutil$/),
+        // Optimistically required in (various) ws versions, with fallback
+        new webpack.IgnorePlugin(/^utf-8-validate$/),
+        // Optimistically required in headless, falls back to child_process
+        new webpack.IgnorePlugin(/^child-killer$/),
+        // Copy Mockttp's schema (read with readFile) into the output directory
+        new CopyWebpackPlugin([
+            { from: path.join('node_modules', 'mockttp', 'dist', 'standalone', 'schema.gql') }
+        ])
+    ],
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    }
+};
