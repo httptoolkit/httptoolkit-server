2d91b14f8e1d2d612258d4d44b1942397a73fa80
Tim Perry <pimterry@gmail.com>
2020-05-08 14:43:26 +0200

    Cleanup old servers on startup

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,6 +2,11 @@
 type ErrorTrackingModule = typeof import('../error-tracking');
 type IndexTypeModule = typeof import('../index');
 
+import * as path from 'path';
+import { promises as fs } from 'fs'
+import * as semver from 'semver';
+import * as rimraf from 'rimraf';
+
 import { IS_PROD_BUILD } from '../constants';
 
 function maybeBundleImport<T>(moduleName: string): T {
@@ -40,6 +45,8 @@ class HttpToolkitServer extends Command {
     async run() {
         const { flags } = this.parse(HttpToolkitServer);
 
+        this.cleanupOldServers(); // Async cleanup old server versions
+
         await runHTK({
             configPath: flags.config,
             authToken: flags.token
@@ -47,6 +54,66 @@ class HttpToolkitServer extends Command {
             await reportError(error);
             throw error;
         });
+    }
+
+    // On startup, we want to kill any downloaded servers that are not longer necessary
+    async cleanupOldServers() {
+        const { dataDir, version: currentVersion } = this.config;
+
+        const serverUpdatesPath = process.env.OCLIF_CLIENT_HOME ||
+            path.join(dataDir, 'client');
+
+        // Be careful - if the server path isn't clearly ours somehow, ignore it.
+        if (!serverUpdatesPath.split(path.sep).includes('httptoolkit-server')) {
+            reportError(`Unexpected server path (${serverUpdatesPath}), ignoring`);
+            return;
+        }
+
+        const serverPaths= await fs.readdir(serverUpdatesPath)
+            .catch((e) => {
+                if (e.code === 'ENOENT') return null;
+                else throw e;
+            });
+        if (!serverPaths) return; // No server update path means we're all good
+
+        // Similarly, if the folder contains anything unexpected, be careful and do nothing.
+        if (serverPaths.some((filename) =>
+            !semver.valid(filename.replace(/\.partial\.\d+$/, '')) &&
+            filename !== 'bin' &&
+            filename !== 'current'
+        )) {
+            console.log(serverPaths);
+            reportError(
+                `Server path (${serverUpdatesPath}) contains unexpected content, ignoring`
+            );
+            return;
+        }
+
+        if (serverPaths.every((filename) => {
+            const version = semver.valid(filename.replace(/\.partial\.\d+$/, ''));
+            return !version || semver.lt(version, currentVersion);
+        })) {
+            // If everything is outdated, just drop the whole folder. Useful if you start
+            // a new server standalone (not just from an update), because otherwise the
+            // update dir can end up in a broken state. Better to clear it completely.
+            console.log("Downloaded server directory is entirely outdated, deleting it");
+            rimraf(serverUpdatesPath, (error) => {
+                if (error) reportError(error);
+            });
+        } else {
+            // Some of the servers are outdated, but not all (maybe it includes us).
+            // Async delete all server versions older than this currently running version.
+            serverPaths.forEach((filename) => {
+                const version = semver.valid(filename.replace(/\.partial\.\d+$/, ''));
+
+                if (version && semver.lt(version, currentVersion)) {
+                    console.log(`Deleting old server ${filename}`);
+                    rimraf(path.join(serverUpdatesPath, filename), (error) => {
+                        if (error) reportError(error);
+                    });
+                }
+            });
+        }
     }
 }
 
