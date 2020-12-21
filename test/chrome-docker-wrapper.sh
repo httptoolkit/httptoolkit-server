53acbf9635cfb4cb2321a08deaf16dea91c43889
Tim Perry <pimterry@gmail.com>
2020-12-21 15:15:53 +0100

    Provide a working test environment for GHA builds

---
+++ test/chrome-docker-wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+trap : SIGTERM SIGINT
+
+# We need to inject a --no-sandbox option, because inside Docker Chrome cannot start its
+# sandbox, and without this it refuses to start at all.
+/usr/bin/google-chrome --no-sandbox "$@" &
+CHROME_PID=$!
+
+wait $CHROME_PID
+
+WAIT_EXIT_CODE=$?
+
+# If this script gets killed, kill Chrome with the same signal
+if [[ WAIT_EXIT_CODE -gt 128 ]]
+then
+    echo kill -$(($WAIT_EXIT_CODE - 128)) $CHROME_PID
+    kill -$(($WAIT_EXIT_CODE - 128)) $CHROME_PID
+fi
