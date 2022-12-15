1a4ca65ff9d6314950f20162b17b4e5c2f406f15
Tim Perry <pimterry@gmail.com>
2022-12-15 11:51:21 +0100

    Update webpack to v5 to support modern JS syntax

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
         'win-version-info', // Native module
         'node-datachannel', // Native module
         'vm2', // Does odd things with require, can't be webpack'd
-        'cpu-features', // Native module, optionally used in ssh2, used within Dockerode, we skip it
+        '_stream_wrap', // Used in httpolyglot only in old Node, where it's available
         function (context, request, callback) {
             if (context !== __dirname && request.endsWith('/error-tracking')) {
                 // Direct all requires of error-tracking to its entrypoint at the top level,
@@ -77,24 +77,35 @@ module.exports = {
     ],
     plugins: [
         // Optimistic require for 'iconv' in 'encoding', falls back to 'iconv-lite'
-        new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, 'node-noop'),
+        new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, /node-noop/),
         // Optimistically required in (various) ws versions, with fallback
-        new webpack.IgnorePlugin(/^bufferutil$/),
+        new webpack.IgnorePlugin({ resourceRegExp: /^bufferutil$/ }),
         // Optimistically required in (various) ws versions, with fallback
-        new webpack.IgnorePlugin(/^utf-8-validate$/),
+        new webpack.IgnorePlugin({ resourceRegExp: /^utf-8-validate$/ }),
         // Optimistically required in headless, falls back to child_process
-        new webpack.IgnorePlugin(/^child-killer$/),
+        new webpack.IgnorePlugin({ resourceRegExp: /^child-killer$/ }),
         // Dev-only require format, used in various adbkit modules
-        new webpack.IgnorePlugin(/^\.\/src\/(adb|logcat|monkey)$/, /adbkit/),
+        new webpack.IgnorePlugin({
+            resourceRegExp: /^\.\/src\/(adb|logcat|monkey)$/,
+            contextRegExp: /adbkit/
+        }),
         // GraphQL playground - required but never used in production bundles
         new webpack.NormalModuleReplacementPlugin(/^\.\/renderGraphiQL$/, 'node-noop'),
+        // SSH2 - used within Dockerode, but we don't support it and it has awkward native deps
+        new webpack.NormalModuleReplacementPlugin(/^ssh2$/, 'node-noop'),
         // CDP protocol - not used without local:true (which we never use, we always
         // get the CDP protocol details from the target Electron app).
-        new webpack.IgnorePlugin(/^\.\/protocol.json$/, /chrome-remote-interface/),
+        new webpack.IgnorePlugin({
+            resourceRegExp: /^\.\/protocol.json$/,
+            contextRegExp: /chrome-remote-interface/
+        }),
         // Copy Mockttp's schema (read with readFile) into the output directory
-        new CopyWebpackPlugin([
-            { from: path.join('node_modules', '@httptoolkit', 'browser-launcher', 'res'), to: 'bl-resources' }
-        ]),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: path.join('node_modules', '@httptoolkit', 'browser-launcher', 'res'),
+                to: 'bl-resources'
+            }]
+        }),
         // If SENTRY_AUTH_TOKEN is set, upload this sourcemap to Sentry
         process.env.SENTRY_AUTH_TOKEN
             ? new SentryPlugin({
