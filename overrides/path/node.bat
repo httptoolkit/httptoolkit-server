00ee967317211e0094f6b5ce18e74bc53b4aef86
Tim Perry <pimterry@gmail.com>
2023-03-24 18:12:50 +0100

    Fix Windows $PATH overrides in powershell, and simplify en route
    We didn't use this var previously since it wasn't set until the
    recent-ish PHP scandir changes, but it's definitely better to just pass
    the path explicitly rather than trying to detect it.
    
    Haven't changed Linux just to avoid churn because it's a little
    complicated to confirm the equivalent and test every possible case, but
    we absolutely could if we ever have issues with the current setup there.

--- overrides/path/node.bat
+++ overrides/path/node.bat
@@ -3,11 +3,7 @@ SETLOCAL
 
 REM Exclude ourselves from PATH within this script, to avoid recursing
 set ORIGINALPATH=%PATH%
-REM Get the current file's folder
-set THIS_PATH=%~dp0
-REM Strip the trailing slash from the folder
-set WRAPPER_FOLDER=%THIS_PATH:~0,-1%
-REM Remove that folder from PATH
+set WRAPPER_FOLDER=%HTTP_TOOLKIT_OVERRIDE_PATH%\path
 call set PATH=%%PATH:%WRAPPER_FOLDER%;=%%
 
 REM Get the real node path, store it in %REAL_NODE%
