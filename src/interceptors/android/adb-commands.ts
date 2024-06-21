httptoolkit/httptoolkit-server

commit 59f013f576ac72d19d84c28662d967f78cc3ff14

Avoid mkdir -m too, to improve backward compat of Android ADB setup
Without this, some older devices (API 22) were failing during automated
setup. That was somewhat hidden by them supporting user certificates,
but still it would be better to use system certificates properly anyway.

src/interceptors/android/adb-commands.ts
@@ -316,7 +316,9 @@ export async function injectSystemCertificate(
 
             # Create a separate temp directory, to hold the current certificates
             # Without this, when we add the mount we can't read the current certs anymore.
-            mkdir -p -m 700 /data/local/tmp/htk-ca-copy
+            mkdir -p /data/local/tmp/htk-ca-copy
+            chown 700 /data/local/tmp/htk-ca-copy
+            rm -rf /data/local/tmp/htk-ca-copy/*
 
             # Copy out the existing certificates
             if [ -d "/apex/com.android.conscrypt/cacerts" ]; then

commit 3f7a92146d0dc85b3a2796b6302fea8ff0793f8c

Fix incorrect command in ADB cert injection script
Fixes: 59f013f5

src/interceptors/android/adb-commands.ts
@@ -410,7 +410,7 @@ export async function injectSystemCertificate(
             # Create a separate temp directory, to hold the current certificates
             # Without this, when we add the mount we can't read the current certs anymore.
             mkdir -p /data/local/tmp/htk-ca-copy
-            chown 700 /data/local/tmp/htk-ca-copy
+            chmod 700 /data/local/tmp/htk-ca-copy
             rm -rf /data/local/tmp/htk-ca-copy/*
 
             # Copy out the existing certificates